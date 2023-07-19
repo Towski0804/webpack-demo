7a9991234468c44fc40698eb727e8097fd3c838d
Towski0804 <superchao0206@gmail.com>
2023-07-19 20:10:10 +0800

    slightly update Jul19-2009

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,10 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 module.exports = {
   mode: "development",
   entry: "./src/index.js",
+  devtool: "inline-source-map",
+  devServer: {
+    static: "./dist",
+  },
   output: {
     path: path.resolve(__dirname, "dist"),
     filename: "index.[contenthash].js",
